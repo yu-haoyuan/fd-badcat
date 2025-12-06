8f3fdba24cf381920b56c9b9de9d6540f8e90d05
yuhaoyuan46 <114276730+yu-haoyuan@users.noreply.github.com>
2025-12-06 22:06:47 +0800

    Check for existing conda environment before creation

--- setup/qwen3omni_env.sh
+++ setup/qwen3omni_env.sh
@@ -2,7 +2,8 @@
 mkdir model
 cd model
 git clone -b qwen3_omni https://github.com/wangxiongts/vllm.git
-conda create -n fdbc-qwen3o-vllm python=3.10 -y
+conda env list | grep -q fdbc-qwen3o-vllm || conda create -n fdbc-qwen3o-vllm python=3.10 -y
+
 conda activate fdbc-qwen3o-vllm
 cd vllm
 pip install -r requirements/build.txt
