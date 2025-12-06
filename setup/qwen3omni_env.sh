7eeeeaee02e1b7b2bb21a5b4c1ac7628b925a5d4
米饭不是配菜 <1662741237@qq.com>
2025-12-06 20:32:45 +0800

    pip install modelscope

--- setup/qwen3omni_env.sh
+++ setup/qwen3omni_env.sh
@@ -10,6 +10,7 @@ pip install -r requirements/cuda.txt
 pip install -e . -v --no-build-isolation
 pip install tokenizers==0.22.1 transformers==4.57.3
 pip install accelerate
+pip install modelscope
 pip install qwen-omni-utils -U
 pip install -U flash-attn --no-build-isolation
 modelscope download --model Qwen/Qwen3-Omni-30B-A3B-Instruct --local_dir ./model/Qwen3-Omni-30B-A3B-Instruct
